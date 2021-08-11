63726a204665f7b023b0ca9a88fd34c189191e0b
Anton Shashkov <frontendmaster@yandex.ru>
2021-08-11 17:00:35 +0500

    fix validation phoneField component

--- components/formFields/PhoneField.vue
+++ components/formFields/PhoneField.vue
@@ -6,11 +6,10 @@
       <span v-if="row.validations[0]" class="required">*</span>
       <span v-if="row.tooltip" class="hint" :title="row.tooltip"></span>
     </label>
-    <ValidationProvider rules="minPhone:17" v-slot="{ errors }">
+    <ValidationProvider :rules="row.validations[0]" v-slot="{ errors }">
       <input
         v-mask="'+7(###)-###-##-##'"
         :id="row.name"
-        :required="row.validations[0]"
         :class="{'is-invalid': errors[0]}"
         type="text"
         :disabled="row.disabled"
